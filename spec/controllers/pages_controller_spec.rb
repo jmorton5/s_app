fa72535e76737a91c6925bbbde6846f5ec570052
John Morton <jmorton1975@gmail.com>
2011-10-07 16:30:24 -0700

    Added a title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,37 +18,33 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-				    :content => "Ruby on Rails Tutorail Sample App |Home")
-    end
-  
-    it "should have a non-blank body" do
-      get 'home'
-      response.body.should_not =- /<body>\s*<\/body>/
+                                    :content => @base_title + " | Home")
     end
   end
-  
+
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
-        it "should have the right title" do
+
+    it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-				    :content => "Ruby on Rails Tutorail Sample App |Home")
+                                    :content => @base_title + " | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-        it "should have the right title" do
+
+    it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-				    :content => "Ruby on Rails Tutorail Sample App |Home")
+                                    :content => @base_title + " | About")
     end
   end
-
 end
