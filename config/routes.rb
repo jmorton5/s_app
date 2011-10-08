e04d6dffd68bb492625c85e300ceb57dbfcd7fed
John Morton <jmorton1975@gmail.com>
2011-10-07 21:23:27 -0700

    Added a Users controller and layout link tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SApp::Application.routes.draw do
   match '/contact' , :to => 'pages#contact'
-  match '/about' , :to => 'pages#about'
-  match '/help' , :to => 'pages#help'
+  match '/about' ,   :to => 'pages#about'
+  match '/help' ,    :to => 'pages#help'
+  match '/signup' ,  :to => 'users#new'
 
   root :to => 'pages#home'
   
