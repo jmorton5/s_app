493b3f813f7e89a718f5f2771b0355d469543744
John Morton <jmorton1975@gmail.com>
2011-11-07 17:42:22 -0800

    Finished user signup

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,12 @@ module SApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initilize_dependency_mechanism do
+	# Work around initializer in ralties/lib/rails/application/bootstrap.rb
+	ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
   end
 end
